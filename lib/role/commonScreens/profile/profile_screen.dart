ba162494d8bcc26c2c0f1c0852c7ce265c2ff28f
siamprodhan <siambci@gmail.com>
2025-12-28 17:47:28 +0600

    splash and auth section is updated

--- lib/role/commonScreens/profile/profile_screen.dart
+++ lib/role/commonScreens/profile/profile_screen.dart
@@ -28,118 +28,125 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GradientScaffold(
-      appBar: AppBar(
-        title: const CommonText(text: 'Profile', fontSize: 18, fontWeight: FontWeight.w500,),
-      ),
       child: SafeArea(
-        child: SingleChildScrollView(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              // Profile Header
-              Container(
-                padding: const EdgeInsets.all(16),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(16),
-                  border: Border.all(color: const Color(0xFFE6E6E6)),
-                ),
-                child: Row(
-                  children: [
-                    const CircleAvatar(
-                      radius: 32,
-                      backgroundImage: NetworkImage(
-                        'https://shorturl.at/WSMrn',
-                      ),
+        child: Column(
+          children: [
+            CommonText(
+              text: 'Profile',
+              fontSize: 18,
+              fontWeight: FontWeight.w500,
+            ),
+            SizedBox(height: 20,),
+            SingleChildScrollView(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // Profile Header
+                  Container(
+                    padding: const EdgeInsets.all(16),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(16),
+                      border: Border.all(color: const Color(0xFFE6E6E6)),
                     ),
-                    const SizedBox(width: 16),
+                    child: Row(
+                      children: [
+                        const CircleAvatar(
+                          radius: 32,
+                          backgroundImage: NetworkImage(
+                            'https://shorturl.at/WSMrn',
+                          ),
+                        ),
+                        const SizedBox(width: 16),
 
-                    const CommonText(
-                      text: 'Borla Ghana',
-                      fontSize: 20,
-                      fontWeight: FontWeight.w600,
-                      textAlign: TextAlign.left,
-                      maxLines: 1,
+                        const CommonText(
+                          text: 'Borla Ghana',
+                          fontSize: 20,
+                          fontWeight: FontWeight.w600,
+                          textAlign: TextAlign.left,
+                          maxLines: 1,
+                        ),
+
+                        const Spacer(),
+                        InkWell(
+                          onTap: () {
+                            Get.to(()=> EditProfileScreen());
+                          },
+                            child: Icon(Icons.chevron_right, color: Colors.grey[600])),
+                      ],
                     ),
+                  ),
 
-                    const Spacer(),
-                    InkWell(
-                      onTap: () {
-                        Get.to(()=> EditProfileScreen());
-                      },
-                        child: Icon(Icons.chevron_right, color: Colors.grey[600])),
-                  ],
-                ),
-              ),
+                  const SizedBox(height: 40),
 
-              const SizedBox(height: 40),
+                  const CommonText(
+                    text: 'Others',
+                    fontSize: 18,
+                    fontWeight: FontWeight.w700,
+                    color: Colors.black87,
+                    textAlign: TextAlign.left,
+                    maxLines: 1,
+                  ),
 
-              const CommonText(
-                text: 'Others',
-                fontSize: 18,
-                fontWeight: FontWeight.w700,
-                color: Colors.black87,
-                textAlign: TextAlign.left,
-                maxLines: 1,
-              ),
+                  const SizedBox(height: 16),
 
-              const SizedBox(height: 16),
-
-              // Settings items
-              SettingsListItem(
-                icon: Icons.lock_outline,
-                title: 'Change Password',
-                onTap: () {
-                  Get.to(()=> EditProfileScreen());
-                },
+                  // Settings items
+                  SettingsListItem(
+                    icon: Icons.lock_outline,
+                    title: 'Change Password',
+                    onTap: () {
+                      Get.to(()=> EditProfileScreen());
+                    },
+                  ),
+                  SettingsListItem(
+                    icon: Icons.language,
+                    title: 'Change Language',
+                    onTap: () {
+                      showLanguageBottomSheet(context);
+                    },
+                  ),
+                  SettingsListItem(
+                    icon: Icons.notifications_outlined,
+                    title: 'Notifications',
+                    onTap: () {
+                      Get.to(()=> NotificationsScreen());
+                    },
+                  ),
+                  SettingsListItem(
+                    onTap: () {
+                      Get.to(()=> AboutUs());
+                    },
+                    icon: Icons.info_outline,
+                    title: 'About Us',
+                  ),
+                  SettingsListItem(
+                    onTap: () {
+                      Get.to(()=> PrivacyPolicyScreen());
+                    },
+                    icon: Icons.privacy_tip_outlined,
+                    title: 'Privacy policy',
+                  ),
+                  SettingsListItem(
+                    onTap: () {
+                      Get.to(()=> TermsOfConditions());
+                    },
+                    icon: Icons.description_outlined,
+                    title: 'Terms & Conditions',
+                  ),
+                  SettingsListItem(
+                    icon: Icons.logout,
+                    title: 'Logout',
+                    titleColor: Colors.red,
+                    iconColor: Colors.red,
+                    onTap: () {
+                      showLogoutBottomSheet(context);
+                    },
+                  ),
+                ],
               ),
-              SettingsListItem(
-                icon: Icons.language,
-                title: 'Change Language',
-                onTap: () {
-                  showLanguageBottomSheet(context);
-                },
-              ),
-              SettingsListItem(
-                icon: Icons.notifications_outlined,
-                title: 'Notifications',
-                onTap: () {
-                  Get.to(()=> NotificationsScreen());
-                },
-              ),
-              SettingsListItem(
-                onTap: () {
-                  Get.to(()=> AboutUs());
-                },
-                icon: Icons.info_outline,
-                title: 'About Us',
-              ),
-              SettingsListItem(
-                onTap: () {
-                  Get.to(()=> PrivacyPolicyScreen());
-                },
-                icon: Icons.privacy_tip_outlined,
-                title: 'Privacy policy',
-              ),
-              SettingsListItem(
-                onTap: () {
-                  Get.to(()=> TermsOfConditions());
-                },
-                icon: Icons.description_outlined,
-                title: 'Terms & Conditions',
-              ),
-              SettingsListItem(
-                icon: Icons.logout,
-                title: 'Logout',
-                titleColor: Colors.red,
-                iconColor: Colors.red,
-                onTap: () {
-                  showLogoutBottomSheet(context);
-                },
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
